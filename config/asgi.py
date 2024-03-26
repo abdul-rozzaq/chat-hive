0f75863a7133ede814972cebcdf7d14d860e7090
Abdurazzoq Abdusalomov <rayyan.dev@mail.ru>
2024-03-26 07:19:03 +0500

    prepare

--- config/asgi.py
+++ config/asgi.py
@@ -1,8 +1,8 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
 import django
 django.setup()
-
-
-import os
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
@@ -12,7 +12,6 @@ from channels.auth import AuthMiddlewareStack
 
 from project import urls
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 django_asgi_app = get_asgi_application()
 
